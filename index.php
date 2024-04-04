94d7d91ec80e3e5d1cc2c0cc1d1023f1a0302fdc
scarlett-troncoso <scar.labussieri@gmail.com>
2024-04-04 17:52:28 +0200

    add while loop without success

--- index.php
+++ index.php
@@ -14,7 +14,70 @@ if ($connection && $connection->connect_error) {
     die;
 }
 
-var_dump($connection);
-$sql = "SELECT * FROM `students`";
+// var_dump($connection);
+// $sql = "SELECT * FROM `students`";
+$sql = "SELECT * FROM `students`
+WHERE YEAR(`date_of_birth`)=1990";
+
 $result = $connection->query($sql);
+
+var_dump($result);
+
+/*
+while ($row = $result -> fetch_assoc()) {
+   // var_dump($row);
+   // var_dump($row['date_of_birth']); 
+
+    ['name' => $name, 'surname' => $surname, 'date_of_birth' => $date_of_birth  ] = $row; // destructuring
+
+    var_dump($name, $surname, $date_of_birth); 
+
+    die;
+}*/
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>index php-mysqli-university</title>
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"/>
+    </head>
+
+    <body>
+        <header>
+            <!-- place navbar here -->
+        </header>
+        <main>
+            <div>
+                <?php while ($row = $result -> fetch_assoc()) :
+                    ['name' => $name, 'surname' => $surname, 'date_of_birth' => $date_of_birth  ] = $row; ?>
+            
+                        <div>
+                            <strong>Name:</strong>
+                            <?php $name . ' ' . $surname?>
+                        </div>
+                        <div>
+                            <strong>Date of birth:</strong>
+                            <?php $date_of_birth ?>
+                        </div>
+                        <hr>
+                    
+                <?php endwhile; ?>
+            </div>
+    
+        </main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        
+    </body>
+</html>
